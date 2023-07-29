5c5b2dfbfba2258d6d83d93c66a80b2ddfc29614
oarthurvil <arthuraquinovil@gmail.com>
2023-07-29 17:46:53 -0300

    Documentation with swagger

--- src/main/java/com/example/inventorymanagement/controller/CategoryController.java
+++ src/main/java/com/example/inventorymanagement/controller/CategoryController.java
@@ -18,35 +18,59 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.inventorymanagement.dto.CategoryDTO;
 import com.example.inventorymanagement.service.CategoryService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 
 @RestController
-@RequestMapping(value = "api/v1/categories")
+@RequestMapping(value = "api/v1/categories", produces = { "application/json" })
 @AllArgsConstructor
 public class CategoryController {
 
     public final CategoryService categoryService;
 
+    @Operation(summary = "Create Category", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Category created")
+    })
     @PostMapping
     public ResponseEntity<Object> saveCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
         categoryService.saveCategory(categoryDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body("Category created.");
     }
 
+    @Operation(summary = "Find Category by ID", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Category found"),
+            @ApiResponse(responseCode = "404", description = "Category not found")
+    })
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Object> findByIdCategory(@PathVariable(value = "id") UUID id) {
+    public ResponseEntity<Object> findByIdCategory(
+            @PathVariable(value = "id") @Parameter(name = "id", description = "Category id", example = "85419402-8372-4008-b378-04c79a61b407") UUID id) {
         CategoryDTO categoryDTO = categoryService.findByIdCategory(id);
         return ResponseEntity.status(HttpStatus.OK).body(categoryDTO);
     }
 
+    @Operation(summary = "Find all Categories", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of Categories found")
+    })
     @GetMapping
-    public ResponseEntity<Object> findAllAddresses() {
+    public ResponseEntity<Object> findAllCategories() {
         List<CategoryDTO> categoriesDTO = categoryService.findAllCategories();
         return ResponseEntity.status(HttpStatus.OK).body(categoriesDTO);
     }
 
+    @Operation(summary = "Delete Category by ID", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Category deleted"),
+            @ApiResponse(responseCode = "404", description = "Category not found")
+    })
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Object> deleteByIdAddress(@PathVariable(value = "id") UUID id) {
+    public ResponseEntity<Object> deleteByIdCategories(
+            @PathVariable(value = "id") @Parameter(name = "id", description = "Category id", example = "85419402-8372-4008-b378-04c79a61b407") UUID id) {
         categoryService.deleteByIdCategory(id);
         return ResponseEntity.status(HttpStatus.OK).body("Category deleted successfully.");
     }
--- src/main/java/com/example/inventorymanagement/controller/SupplierController.java
+++ src/main/java/com/example/inventorymanagement/controller/SupplierController.java
@@ -18,35 +18,59 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.inventorymanagement.dto.SupplierDTO;
 import com.example.inventorymanagement.service.SupplierService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 
 @RestController
-@RequestMapping(value = "api/v1/suppliers")
+@RequestMapping(value = "api/v1/suppliers", produces = { "application/json" })
 @AllArgsConstructor
 public class SupplierController {
-    
+
     public final SupplierService supplierService;
 
+    @Operation(summary = "Create Supplier", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Supplier created")
+    })
     @PostMapping
     public ResponseEntity<Object> saveSupplier(@Valid @RequestBody SupplierDTO supplierDTO) {
-        supplierService.saveSupplier(supplierDTO);        
+        supplierService.saveSupplier(supplierDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body("Supplier created.");
     }
 
+    @Operation(summary = "Find Supplier by ID", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Supplier found"),
+            @ApiResponse(responseCode = "404", description = "Supplier not found")
+    })
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Object> findByIdSupplier(@PathVariable(value = "id") UUID id) {
+    public ResponseEntity<Object> findByIdSupplier(
+            @PathVariable(value = "id") @Parameter(name = "id", description = "Supplier id", example = "85419402-8372-4008-b378-04c79a61b407") UUID id) {
         SupplierDTO supplierDTO = supplierService.findByIdSupplier(id);
         return ResponseEntity.status(HttpStatus.OK).body(supplierDTO);
     }
 
+    @Operation(summary = "Find all Suppliers", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of Suppliers found")
+    })
     @GetMapping
     public ResponseEntity<Object> findAllSuppliers() {
         List<SupplierDTO> suppliersDTO = supplierService.findAllSuppliers();
         return ResponseEntity.status(HttpStatus.OK).body(suppliersDTO);
     }
 
+    @Operation(summary = "Delete Supplier by ID", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Supplier deleted"),
+            @ApiResponse(responseCode = "404", description = "Supplier not found")
+    })
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Object> deleteByIdSupplier(@PathVariable(value = "id") UUID id) {        
+    public ResponseEntity<Object> deleteByIdSupplier(
+            @PathVariable(value = "id") @Parameter(name = "id", description = "Supplier id", example = "85419402-8372-4008-b378-04c79a61b407") UUID id) {
         supplierService.deleteByIdSupplier(id);
         return ResponseEntity.status(HttpStatus.OK).body("Supplier deleted successfully.");
     }
